c0a8ce89af360bf0acef2fdd4a27de8af7ed55b8
Eliana Neurohr <elianajneu@gmail.com>
2019-11-08 15:59:18 -0700

    gun stats parser fixed and CSVs

--- gun_parser.py
+++ gun_parser.py
@@ -1,23 +1,28 @@
 import csv
 
-def parse(filename):
+def parse(filename, output):
 	file_arr = []
+	file = 'gun_stats_2014.csv'
 	with open(filename) as file_object:
 		for line in file_object:
 			line = line.replace(' ','');
 			line = line.replace('||','|');
 			file_arr = line.split("|")
-	with open ('gun_stats.csv', 'w',newline='') as csvfile:
+	with open (output, 'w',newline='') as csvfile:
 		spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
 		spamwriter.writerow(['Lat'] + ['Long']+ ['Incident_Number'])
 		# for row in range(len(file_arr)):
 		# 	spamwriter.writerow(file_arr[row])
-		for x in range(len(file_arr)):
+		x = 0
+		while x < (len(file_arr)):
 			try:
 				spamwriter.writerow([file_arr[x]] + [file_arr[x+1]] + [file_arr[x+2]])
-				x += 3
 			except:
 				csvfile.close()
+			x += 3
 
 
-parse("gun.txt")
+parse("gun_2014.txt", 'gun_stats_2014.csv')
+parse("gun_2015.txt", "gun_stats_2015.csv")
+parse("gun_2016.txt", "gun_stats_2016.csv")
+parse("gun_2017.txt", "gun_stats_2017.csv")
